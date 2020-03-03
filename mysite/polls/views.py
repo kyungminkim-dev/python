c8d83ad929cbdebcafa3ce50f1b1135981fe0399
Kyungmin Kim <57028248+kyungminkim-dev@users.noreply.github.com>
2020-03-04 01:31:09 +0900

    Add files via upload

---
+++ mysite/polls/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from .models import Question, Choice
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+# Create your views here.
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 에러 메세지와 함께 폼을 다시 디스플레이합니다.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # POST 데이터 처리를 정상적으로 마친 뒤에는 항상 HttpResponseRedirect를 리턴합니다.        
+        # 이 방법을 통해 유저가 브라우저의 "뒤로가기"을 눌렀을 때
+        # 데이터가 두 번 저장되는 것을 방지할 수 있습니다.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
